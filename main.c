12f241f24551017bbc7bd3592e65a9fb810c45c4
sexysoushi <barbara1603@hotmail.com>
2014-11-23 15:47:37 +0100

    Idees de variables de base pour commencer

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* Application of the Kanban's method
  * 
- * Authors : Renaud Guillaume - Schiavi Barbara
+ * Authors : Renaud Guillaume - Schiavi Barbara <= Espece de pas galant !!
  * Last modification : November 20, 2014
 */
 
@@ -12,7 +12,22 @@
 #include <time.h>
 #include "structures.h"
 
+/*Idées : */
+//Donc pour les semaphores:
+//- un mutex
+//- quantité d'ordre
+//- carte magnetique
 
+
+//int : nb produits finis (Ressources Critiques)
+
+/* Homme flux (thread) récupere aléatoirement des :*/
+/* Etiquette/carte (thread) contient ordre de fabrication*/
+/* Disposé dans un tableau de lancement (thread)*/
+
+/* Point de départ => stock de constituants (thread)*/
+
+//qu'en penses tu ?
 
 void traitantSIGINT(int s)
 {
