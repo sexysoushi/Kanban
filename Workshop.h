6d6c399968ffacf28c297ebf4c9ba2e4df11609d
sexysoushi <barbara1603@hotmail.com>
2014-11-25 16:44:04 +0100

    le strcat ne fonctionne pas

---
+++ Workshop.h
@@ -0,0 +1,6 @@
+#ifndef WORKSHOP_H
+#define	WORKSHOP_H
+
+void* Workshop_thread_fct(void* arg);
+
+#endif //WORKSHOP_H
--- main.c
+++ main.c
@@ -7,11 +7,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 #include <semaphore.h>
 #include <pthread.h>
 #include <unistd.h>
-#include "Structures.h"
 #include "Workshop.h"
+
+#define NB_SEM 99
+sem_t semTab[NB_SEM];
 
 /* Function used to post the error messages */
 void error(const char* msg) { perror(msg); exit(-1); }
@@ -21,26 +24,44 @@ void applicateWhenSIGINT(int s) { printf("\nProblem : signal stop\n"); exit(-1);
  
 int main(int argc, char* argv[])
 {	
-	int nbFinishProducts;
+	int nbProduitDemande, i;
 	pthread_t t1, t2,t3;
+	char nb[50];	
+	char *semName, *txt;
+
+	/* Noms semaphores*/
+	sprintf(txt, "sem");
+	sprintf(nb, "%d", i);
+	semName = strcat(txt, nb);
 	
 	signal(SIGINT,applicateWhenSIGINT);
 	
 	/* Allocations */
+	txt = (char*) malloc(50*sizeof(char));
 	
-	/* Initialisation : conditions, mutex, available pieces */
-	
-	/*Threads creation */
-	//if(pthread_create(&t1, NULL, Supplier_thread_fct, 0) != 0) { error("Error Supplier thread creation\n"); exit(1);}
-	//if(pthread_create(&t2, NULL, Step_thread_fct, 0) != 0) { error("Error Step thread creation\n"); exit(1);}
-	//if(pthread_create(&t3, NULL, Finish_thread_fct, 0) != 0) { error("Error Finish thread creation\n"); exit(1);}
-	
+	/* Initialisation : semaphores, conditions, mutex, available pieces 
+	for(i = 0; i<NB_SEM; i++)
+	{
+		semTab[i] = sem_open(semName, O_RDWR|O_CREAT, 0666, 0);
+	}
+	*/
 	/* Client's choice */
 	printf("How many products do you want ?\n");
-	scanf("%d", &nbFinishProducts);	
-	printf("Preparation of the order in progress (%d products) ...\n", nbFinishProducts);
+	scanf("%d", &nbProduitDemande);	
+	printf("Preparation of the order in progress (%d products) ...\n", nbProduitDemande);
+
+	/*Threads creation workshop*/
+	//if(pthread_create(&t1, NULL, Workshop_thread_fct, 0) != 0) { error("Error Workshop thread creation\n"); exit(1);}
+	//if(pthread_create(&t2, NULL, Workshop_thread_fct, 0) != 0) { error("Error Workshop thread creation\n"); exit(1);}
+	//if(pthread_create(&t3, NULL, Workshop_thread_fct, 0) != 0) { error("Error Workshop thread creation\n"); exit(1);}
 	
-	sleep(5);
-	
-	return 0;
+	sleep(5); //?
+
+	/*Suppression semaphores
+	for(i = 0; i<NB_SEM; i++)
+	{
+		semTab[i] = sem_unlink(semName);
+	}
+*/
+	pthread_exit(NULL); // le main se détruit mais pas les threads encore en cours
 }
