8aaebc83e1bdc627970a4453010698926e55862c
sexysoushi <barbara1603@hotmail.com>
2014-12-04 15:06:59 +0100

    mise à jour

--- Structures.h
+++ Structures.h
@@ -1,6 +1,7 @@
 #ifndef STRUCTURES_H
 #define	STRUCTURES_H
 
+//une carte magnetique
 typedef struct {
 	char* workshop_name;
 	int nbmax_pieces_container;
@@ -10,24 +11,44 @@ typedef struct {
 	int num_order;
 } Card;
 
+//un container possede une carte magnetique, un nb de piece
 typedef struct {
+	//carte magnetique avec une reference
 	Card magnetic_card;
-	int nb_pieces;
+	int nB_pieceByContainer = 5;
+
 }Container;
 
+//un stock est situé à proximité de chaque poste
+//un stock est constitué de plusieurs conteneur
 typedef struct {
-	int nb_container;
-	Container container;
+	int nB_container = 5;
+	//tableau de container
+	Container tabContainer[nB_container];
+
 }Stock;
 
+//chaque poste possede une BAL
 typedef struct {
+	//le nom du poste où elle se trouve
+	//comment on recupere le nom du poste? 
+
 	//cardList cl;
 } BAL;
+
+
+//peut etre pas besoin d'une struture pour ça
+typedef struct {
+	Card card;
+
+}TdeL; //Tableau de Lancement
 
 typedef struct {
 	BAL Bal;
 	Stock stock;
 	char* name;
+	List<Indications> indications;
+	//une localisation
 	
 } Workshop;
 
--- main.c
+++ main.c
@@ -14,7 +14,6 @@
 #include <pthread.h>
 #include <unistd.h>
 #include "Workshop.h"
-#include "List.h"
 
 #define NB_SEM 99
 
@@ -26,48 +25,50 @@ void error(const char* msg) { perror(msg); exit(-1); }
 
 /* Function used as the mask of the signal SIGINT */
 void applicateWhenSIGINT(int s) { printf("\nProblem : received stop signal !\n"); exit(-1); }
- 
+
 int main(int argc, char* argv[])
-{	
+{
 	int nbProduitDemande, i;
 	pthread_t t1, t2,t3;
-	char nb[10];	
+	char nb[10];
 	char *semName, *txt;
-	
+
 	/* Allocations */
 	txt = (char*) malloc(50*sizeof(char));
-	
+
 	/* Semaphore's name */
 	sprintf(txt, "sem");
 	sprintf(nb, "%d", i);
 	semName = strcat(txt, nb);
 
 	/* Initialization : semaphores, conditions, mutex, available pieces */
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&cond1, NULL);
+	pthread_cond_init(&cond2, NULL);
 	for(i = 0; i<NB_SEM; i++)
 	{
 		semTab[i] = sem_open(semName, O_RDWR | O_CREAT, 0666, 0);
 	}
-		
+
 	signal(SIGINT,applicateWhenSIGINT);
-	
-	
+
 	/* Client's choice */
 	printf("How many products do you want ?\n");
-	scanf("%d", &nbProduitDemande);	
+	scanf("%d", &nbProduitDemande);
 	printf("Preparation of the order in progress (%d products) ...\n", nbProduitDemande);
 
 	/*Threads creation workshop*/
 	//if(pthread_create(&t1, NULL, Workshop_thread_fct, 0) != 0) { error("Error Workshop thread creation\n"); exit(-1);}
 	//if(pthread_create(&t2, NULL, Workshop_thread_fct, 0) != 0) { error("Error Workshop thread creation\n"); exit(-1);}
 	//if(pthread_create(&t3, NULL, Workshop_thread_fct, 0) != 0) { error("Error Workshop thread creation\n"); exit(-1);}
-	
+
 	/*Delete semaphores */
 	for(i = 0; i<NB_SEM; i++)
 	{
 		sem_unlink(semName);
 	}
-	
+
 	free(txt); /* Free the memory allocated to txt with malloc */
-	
+
 	pthread_exit(NULL); /* Destroy the main but not the threads in progress */
 }
